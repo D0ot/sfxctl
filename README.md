e54e6cf20480e670a62f9dcc6087b15f197968e7
doot <gugegby@gmail.com>
2022-08-11 00:15:09 +0800

    1. make all functions into one script. 2.update readme.

--- README.md
+++ README.md
@@ -5,5 +5,45 @@ Functions:
 
 1. Acer mode switch, slient, normal and performance are supported like things under Winodws, also able to enable max fan speed.
 2. Turn off dGPU totally by an acpi call. helpful when on battery.
-3. Brightness control, helpful for WM users.
+3. Brightness control, helpful for Window Manager users.
 4. Show power comsuption infomation reported by battery.
+
+---
+
+## Acer mode switch
+
+Usages:
+```
+./sfx14-41g.sh [m|mode] [s|n|p|m]
+
+```
+s is Slient, n is Normal, p is Performance, m is Max fan speed.
+
+optional dependency: ryzenadj
+
+
+## Turn off dGPU
+
+Usages:
+
+```
+./sfx14-41g.sh [g|gpu] [on|off]
+
+```
+
+dependency: acpi_call-dkms
+
+
+## Brightness control
+
+```
+./sfx14-41g.sh [b|bl] [u|d|up|down]
+```
+
+you should resolve permission probelm yourself.
+
+## Show power comsuption
+
+```
+./sfx14-41g.sh [p]
+```
--- sfx14-41g.sh
+++ sfx14-41g.sh
@@ -1,7 +1,5 @@
 #!/bin/env sh
 
-readonly SCRIPTS_HOME="$HOME/programs/swiftx-fn"
-
 readonly BAT_PATH="/sys/class/power_supply/BAT1"
 
 readonly GPU_OFF_METHOD="\_SB.PCI0.GPP0.PG00._OFF"
@@ -94,7 +92,7 @@ blctl_print_cur()
     echo "Current brightness : $cur_bl / 255"
 }
 
-blctl () 
+blctl ()
 {
     if [ $# -gt 1 ]
     then
@@ -107,7 +105,7 @@ blctl ()
         blctl_print_cur
         exit 1
     fi
-    
+
     local cur_bl=$(cat ${BRIGHTNESS_READ})
 
     local val=$(echo $1 | bc)
@@ -145,9 +143,67 @@ bl ()
 
 }
 
+acer_mode_switch()
+{
+    local MODE=$(sudo ec_probe read 16 | cut -d ' ' -f 1)
+    local MODE_STR=""
+
+    local NEXT_MODE_VAL=""
+    local NEXT_MODE_STR=""
+
+    case "$MODE" in
+    	2) MODE_STR="Slient"
+    	;;
+    	3) MODE_STR="Performance"
+    	;;
+    	0) MODE_STR="Normal"
+    	;;
+    	*) echo "Unknow Current MODE Value:${MODE}, exit without changing anything"
+    		exit 1
+    	;;
+    esac
+
+    echo "Current Mode: $MODE_STR"
+
+    local FAN_SPEED=$(sudo ec_probe read 20 | cut -d ' ' -f 1)
+    echo "Fan speed values: $FAN_SPEED"
+
+    local STAMP_LIMIT=0
+    local FAST_LIMIT=0
+    local SLOW_LIMIT=0
+    
+    case "$1" in
+	    0|"Normal"|"normal"|"n") NEXT_MODE_STR="Normal"; NEXT_MODE_VAL=0
+            STAMP_LIMIT=35000; FAST_LIMIT=35000; SLOW_LIMIT=35000;
+	    ;;
+	    2|"Silent"| "silent"| "s") NEXT_MODE_STR="Silent"; NEXT_MODE_VAL=2
+            STAMP_LIMIT=10000; FAST_LIMIT=10000; SLOW_LIMIT=10000;
+	    ;;
+	    3|"Performance"| "Performance"| "p") NEXT_MODE_STR="Performance"; NEXT_MODE_VAL=3
+            STAMP_LIMIT=50000; FAST_LIMIT=50000; SLOW_LIMIT=50000;
+	    ;;
+	    "max"|"m") sudo ec_probe write 34 8; echo "Maximize fan speed"; exit 0
+	    ;;
+        "")
+            exit 0
+        ;;
+	    *) echo "Unknow Input Mode : "$1", exit without changing anything."
+		    exit 2
+	    ;;
+    esac
+    
+    sudo ec_probe write 34 0
+    echo "Switch to $NEXT_MODE_STR Mode"
+    echo "Mode Value is $NEXT_MODE_VAL"
+    
+    sudo ec_probe write 16 $NEXT_MODE_VAL
+    sleep 1
+    sudo ryzenadj -a $STAMP_LIMIT -b $FAST_LIMIT -c $SLOW_LIMIT
+}
+
 mode ()
 {
-    ${SCRIPTS_HOME}/acer-mode-switch.sh $@
+    acer_mode_switch $@
 }
 
 
